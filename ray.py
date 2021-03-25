4e6290c3c8e3cb6e2c9574d39012981f5f71108a
Anton Sidorov <sidorov@mail.ioffe.ru>
2021-03-25 21:13:10 +0300

    Ray class. Variables renaming

--- ray.py
+++ ray.py
@@ -7,7 +7,7 @@ class Ray:
     def __init__(self, x0=0, y0=0, kx0=0, ky0=1, freq=135e9):
         self.x = np.zeros(10000)
         self.y = np.zeros(10000)
-        self.l = np.zeros(10000)
+        self.L = np.zeros(10000)
         self.kx = np.zeros(10000)
         self.ky = np.zeros(10000)
         self.ph = np.zeros(10000)
@@ -50,34 +50,34 @@ class Ray:
         ny = len(n_field['y'])
 
         # grid step
-        dX = n_field['x'][1] - n_field['x'][0]
-        dY = n_field['y'][1] - n_field['y'][0]
+        dx = n_field['x'][1] - n_field['x'][0]
+        dy = n_field['y'][1] - n_field['y'][0]
 
         # gradient arrays
-        dNx = (n_field['n'][0:-1, 1:] - n_field['n'][0:-1, 0:-1]) / dX
-        dNy = (n_field['n'][1:, 0:-1] - n_field['n'][0:-1, 0:-1]) / dY
+        dn_x = (n_field['n'][0:-1, 1:] - n_field['n'][0:-1, 0:-1]) / dx
+        dn_y = (n_field['n'][1:, 0:-1] - n_field['n'][0:-1, 0:-1]) / dy
 
         # step determination
-        dXYmin = min(dX, dY) / const.golden_ratio
-        Amax = max(np.max(abs(dNx)), np.max(abs(dNy))) / 2 / nc * w ** 2 / c ** 2
-        Bmax = w / c
-        t = min((-Bmax + np.sqrt(Bmax ** 2 + 4 * Amax * dXYmin)) / 2 / Amax, dXYmin * c / w)
+        min_xy = min(dx, dy) / const.golden_ratio
+        a_max = max(np.max(abs(dn_x)), np.max(abs(dn_y))) / 2 / nc * w ** 2 / c ** 2
+        b_max = w / c
+        t = min((-b_max + np.sqrt(b_max ** 2 + 4 * a_max * min_xy)) / 2 / a_max, min_xy * c / w)
 
-        # intialization of ray
+        # initialization of ray
         # convert to grid coordinates
-        Xinterpolant = interp1d(n_field['x'], np.arange(0, nx))
-        Yinterpolant = interp1d(n_field['y'], np.arange(0, ny))
-        Ninterpolant = interp2d(n_field['x'], n_field['y'], n_field['n'], kind='cubic')
-        refrIdx = np.sqrt(1 - Ninterpolant(self.x[0], self.y[0]) / nc)
-        self.kx[0] = self.kx[0] * refrIdx
-        self.ky[0] = self.ky[0] * refrIdx
-        curX = Xinterpolant(self.x[0])
-        curY = Yinterpolant(self.y[0])
+        x_interpolant = interp1d(n_field['x'], np.arange(0, nx))
+        y_interpolant = interp1d(n_field['y'], np.arange(0, ny))
+        n_interpolant = interp2d(n_field['x'], n_field['y'], n_field['n'], kind='cubic')
+        refr = np.sqrt(1 - n_interpolant(self.x[0], self.y[0]) / nc)
+        self.kx[0] = self.kx[0] * refr
+        self.ky[0] = self.ky[0] * refr
+        x_cur = x_interpolant(self.x[0])
+        y_cur = y_interpolant(self.y[0])
 
         i = 0
-        while 0 <= curX and nx - 1 > curX and 0 <= curY and ny - 1 > curY:
-            self.ax[i] = -dNx[int(np.fix(curY)), int(np.fix(curX))] / 2 / nc * w ** 2 / c ** 2
-            self.ay[i] = -dNy[int(np.fix(curY)), int(np.fix(curX))] / 2 / nc * w ** 2 / c ** 2
+        while 0 <= x_cur < nx - 1 and 0 <= y_cur < ny - 1:
+            self.ax[i] = -dn_x[int(np.fix(y_cur)), int(np.fix(x_cur))] / 2 / nc * w ** 2 / c ** 2
+            self.ay[i] = -dn_y[int(np.fix(y_cur)), int(np.fix(x_cur))] / 2 / nc * w ** 2 / c ** 2
 
             self.kx[i + 1] = self.kx[i] + self.ax[i] * t
             self.ky[i + 1] = self.ky[i] + self.ay[i] * t
@@ -88,12 +88,12 @@ class Ray:
             k = np.sqrt(self.kx[i] ** 2 + self.ky[i] ** 2)
             k1 = np.sqrt(self.kx[i + 1] ** 2 + self.ky[i + 1] ** 2)
 
-            l = t * (k1 + k) / 2
-            self.ph[i + 1] = self.ph[i] + l * (k1 + k) / 2
-            self.l[i + 1] = self.l[i] + l
+            dl = t * (k1 + k) / 2
+            self.ph[i + 1] = self.ph[i] + dl * (k1 + k) / 2
+            self.L[i + 1] = self.L[i] + dl
 
-            curX = curX + (self.kx[i] * t + self.ax[i] * t ** 2 / 2) / dX
-            curY = curY + (self.ky[i] * t + self.ay[i] * t ** 2 / 2) / dY
+            x_cur = x_cur + (self.kx[i] * t + self.ax[i] * t ** 2 / 2) / dx
+            y_cur = y_cur + (self.ky[i] * t + self.ay[i] * t ** 2 / 2) / dy
             i += 1
 
         self.x = self.x[:i]
@@ -102,7 +102,7 @@ class Ray:
         self.ky = self.ky[:i]
         self.ax = self.ax[:i]
         self.ay = self.ay[:i]
-        self.l = self.l[:i]
+        self.L = self.L[:i]
         self.ph = self.ph[:i]
 
         stop = timeit.default_timer()
