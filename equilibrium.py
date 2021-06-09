df37bff6b63af72fdc37fc6c6943c3802bd265b1
Anton Sidorov <sidorov@mail.ioffe.ru>
2021-06-09 18:57:44 +0300

    working with geometry refactored in equilibrium.py

--- equilibrium.py
+++ equilibrium.py
@@ -1,6 +1,7 @@
 import numpy as np
+from numpy.polynomial import Polynomial as poly
 import scipy.interpolate as interp
-import pandas
+from scipy.optimize import minimize_scalar
 
 
 class Equilibrium:
@@ -42,7 +43,85 @@ class Equilibrium:
                                                       self.psi, kx=1, ky=1)
 
 
-def read_asta_tab(filename):
+class RXYTE:
+    """
+        Class represents geometry of psi through shifts, elongation and triangulation
+    """
+    _n_theta_max = 1000
+
+    def __init__(self, r, x=None, y=None, tria=None, elon=None):
+        if isinstance(r, np.ndarray):
+            if r.ndim == 2:
+                if r.shape[1] >= 5:
+                    r, x, y, tria, elon = (r[:, i] for i in range(5))
+        elif isinstance(r, (int, np.int64)):
+            (self.r, self.x, self.y, self.tria) = (np.zeros((r,)) for _ in range(4))
+            self.elon = np.ones((r, 1))
+        elif isinstance(r, (tuple, list)):
+            if len(r) == 2:
+                self.r = np.linspace(0, r[0], r[1])
+                (self.x, self.y, self.tria) = (np.zeros((r[1],)) for _ in range(3))
+                self.elon = np.ones((r[1], 1))
+        ind = np.argsort(r)
+        self.r = r[ind]
+        self.x = x[ind]
+        self.y = y[ind]
+        self.tria = tria[ind]
+        self.elon = elon[ind]
+
+    def self_polar(self, theta=None):
+
+        if theta is None:
+            grid_step = np.diff(self.r).min()
+            theta = np.round(2 * np.pi * self.r.max() / grid_step).astype(np.int64)
+            theta = min(RXYTE._n_theta_max, theta)
+
+        if isinstance(theta, (int, np.int64)):
+            n_theta = theta
+            theta = np.linspace(0, 2 * np.pi, n_theta + 1)[:-1]
+
+        r, theta = np.meshgrid(self.r, theta)
+
+        return r, theta
+
+    def self_cartesian(self, theta=None):
+        r, theta = self.self_polar(theta=theta)
+        xx = self.x + r * (np.cos(theta) - self.tria * np.sin(theta)**2)
+        yy = self.y + r * self.elon * np.sin(theta)
+        return xx, yy
+
+    @property
+    def nested(self):
+        xx, yy = self.self_cartesian()
+        inout = 1
+        for ri in range(len(self.r) - 1):
+            inout *= -check_inside((xx[:, ri] - self.x[ri], yy[:, ri] - self.y[ri]),
+                                   (xx[:, ri + 1] - self.x[ri], yy[:, ri + 1] - self.y[ri]))
+        if inout == 1:
+            return True
+        else:
+            return False
+
+    def r_lcs(self, limiter):
+        xx, yy = self.self_cartesian()
+        limiter = to_complex(limiter)
+        ri = 0
+        while ri<len(self.r) and \
+              check_inside((xx[:, ri]-self.x[ri], yy[:, ri] - self.y[ri]),
+                           (limiter.real-self.x[ri], limiter.imag-self.y[ri])):
+            ri += 1
+        if ri < len(self.r)-1:
+            rlcs, lcs = rlcs_from_params(shifx=lc(self.r[ri], self.r[ri+1], self.x[ri], self.x[ri+1]),
+                                         shify=lc(self.r[ri], self.r[ri+1], self.y[ri], self.y[ri+1]),
+                                         trian=lc(self.r[ri], self.r[ri+1], self.tria[ri], self.tria[ri+1]),
+                                         elon=lc(self.r[ri], self.r[ri + 1], self.elon[ri], self.elon[ri + 1]),
+                                         limiter=limiter)
+        else:
+            rlcs, lcs = rlcs_from_params(self.x[-1], self.y[-1], trian=self.tria[-1], elon=self.elon[-1], limiter=limiter)
+
+        return rlcs, lcs
+
+def read_astra_tab(filename):
     with open(filename,'r') as fh:
         lines = list()
         for ln in fh.readlines():
@@ -90,3 +169,108 @@ def read_asta_tab(filename):
             tbl[key] = value
 
     return tbl
+
+
+def rlcs_from_params(shifx=0., shify=0., trian=0., elon=1., limiter=7.85):
+
+    limiter = to_complex(limiter)
+
+    shifx = poly(shifx)
+    shify = poly(shify)
+    trian = poly(trian)
+    elon = poly(elon)
+
+    theta = np.angle(limiter)
+    lcs = lambda r: (shifx(r) + r * (np.cos(theta) - trian(r) * np.sin(theta) ** 2)) + \
+                    1j * (shify(r) + r * elon(r) * np.sin(theta))
+
+    def dist_from_limiter(lmt, lcs1):
+        if all(np.abs(lcs1) <= np.abs(lmt)):
+            return (np.abs(lmt) - np.abs(lcs1)).min()
+        else:
+            return np.inf
+
+    rlcs = minimize_scalar(lambda r: dist_from_limiter(limiter, lcs(r)),
+                           bracket=(0, np.abs(limiter).max()),
+                           bounds=(0, np.abs(limiter).max()),
+                           args=(), method='bounded', tol=None,
+                           options={'xatol': 1e-4, 'maxiter': 5000, 'disp':0}).x
+    return rlcs, lcs(rlcs)
+
+def rxyte_from_params(shifx=0., shify=0., trian=0., elon=1., num=100, limiter=7.85, length_unit='cm'):
+
+    rlcs, lcs = rlcs_from_params(shifx, shify, trian, elon, limiter)
+
+    # convert inputs to polynomial objects
+    shifx = poly(shifx)
+    shify = poly(shify)
+    trian = poly(trian)
+    elon = poly(elon)
+
+    rxyte = np.zeros((num,5))
+    rxyte[:, 0] = np.linspace(0, rlcs, num)
+    rxyte[0, 0] = 1e-4
+    rxyte[:, 1] = shifx(rxyte[:, 0])
+    rxyte[:, 2] = shify(rxyte[:, 0])
+    rxyte[:, 3] = trian(rxyte[:, 0])
+    rxyte[:, 4] = elon(rxyte[:, 0])
+
+    return RXYTE(rxyte), rlcs, lcs
+
+
+def check_inside(a, b):
+    # for this algorithm origin must be inside of convex contour
+    # for working in polar coordinates make complex numbers
+    a = to_complex(a)
+    b = to_complex(b)
+
+    ang2rho = interp.interp1d(np.angle(b), np.abs(b), kind='cubic', bounds_error=False,
+                       fill_value=(np.abs(b[0]), np.abs(b[-1])))
+
+    # radius coordinates of "b" on same angels as in "a"
+    rb = ang2rho(np.angle(a))
+
+    if all(np.abs(a) < rb):
+        return -1  # a is inside b
+    elif all(np.abs(a) > rb):
+        return 1  # a is outside b
+    else:
+        return 0
+
+
+def rxyte_from_astra_tab(tbl):
+    # TODO change names of parameters in astra
+    return RXYTE(tbl['a'], x=tbl['shif'], y=tbl['shiv'], tria=tbl['tria'], elon=tbl['elon'])
+
+
+def geqdsk_from_astra_tab(tbl, rmain=0.55, rdia=0.0785):
+    geom = rxyte_from_astra_tab(tbl)
+    geqdsk = dict()
+    geqdsk['nx'] = 65
+    geqdsk['ny'] = 65
+    geqdsk['rdim'] = 0.2
+    geqdsk['zdim'] = 0.2
+    geqdsk['rcentr'] = rmain
+    geqdsk['rleft'] = rmain - geqdsk['rdim']/2
+    geqdsk['zmid'] = 0.
+    geqdsk['rmagx'] = geom.psi2x(0) + rmain
+    geqdsk['zmagx'] = geom.psi2y(0)
+    geqdsk['simagx'] = geom.psi.min()
+
+
+def to_complex(curve):
+    if np.isscalar(curve):
+        theta = np.linspace(0, 2 * np.pi, RXYTE._n_theta_max, endpoint=False)
+        curve = curve * np.cos(theta) + 1j * curve * np.sin(theta)
+    elif isinstance(curve, (tuple, list)):
+        if len(curve) == 2:
+            curve = curve[0] + 1j * curve[1]
+    elif isinstance(curve, np.ndarray):
+        if curve.ndim == 2:
+            curve = curve[:, 0] + 1j * curve[:, 1]
+    curve = curve[np.argsort(np.angle(curve))]
+    return curve
+
+
+def lc(x1,x2,y1,y2):
+    return (x1*y2-x2*y1)/(x1-x2), (y1-y2)/(x1-x2)
